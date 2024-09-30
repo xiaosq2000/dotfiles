d3a502f405c1b860bcf6d76ab7c4423db14d961c
Shuqi XIAO <xiaosq2000@gmail.com>
2024-09-30 13:21:43 +0800

    Update

--- .config/nvim/lua/core/LuaSnip/dockerfile/docker.lua
+++ .config/nvim/lua/core/LuaSnip/dockerfile/docker.lua
@@ -17,8 +17,13 @@ local get_visual = function(args, parent)
     end
 end
 return {
-    s({ trig = "build" }, fmta([[
-# Build & Install <>
+    s({trig="apt"}, fmta([[
+sudo apt-get update && sudo apt-get install -qy --no-install-recommends \
+<> && \
+sudo rm -rf /var/lib/apt/lists/* && \
+    ]], {i(1, "packages")})),
+    s({ trig = "build-local" }, fmta([[
+# build & install <>
 ARG <>_VERSION
 COPY --chown=${DOCKER_USER}:${DOCKER_USER} ./downloads/<>-${<>_VERSION}.tar.gz .
 RUN mkdir <>-${<>_VERSION} && tar -zxf <>-${<>_VERSION}.tar.gz --strip-component=1 -C <>-${<>_VERSION} && \
@@ -30,4 +35,16 @@ RUN mkdir <>-${<>_VERSION} && tar -zxf <>-${<>_VERSION}.tar.gz --strip-component
     ]],
         { i(1, "eigen"), i(2, "EIGEN"), rep(1), rep(2), rep(1), rep(2), rep(1), rep(2), rep(1), rep(2), rep(1), rep(2),
             rep(1) })),
+    s({ trig = "build-git" }, fmta([[
+# build & install <>
+ARG <>_GIT_REFERENCE
+RUN git clone --config http.proxy="${http_proxy}" --config https.proxy="${https_proxy}" "<>" <> && \
+    cd <> && \ 
+    git checkout ${<>_GIT_REFERENCE} && \
+    cmake -B build -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_STANDARD=17 && \
+    cmake --build build -j ${COMPILE_JOBS} && \
+    cmake --install build --prefix ${XDG_PREFIX_HOME} && \
+    rm -r <>*
+    ]],
+        { i(1, "eigen"), i(2, "EIGEN"), i(3, "url"), rep(1), rep(1), rep(2), rep(1) })),
 }
