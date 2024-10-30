b3dca040c005c9ca3555bff98a911ec8a9d07661
Shuqi XIAO <xiaosq2000@gmail.com>
2024-10-30 02:30:07 +0000

    Update

--- .config/nvim/lua/core/plugins.lua
+++ .config/nvim/lua/core/plugins.lua
@@ -465,7 +465,9 @@ local plugins = {
         'windwp/nvim-autopairs',
         event = "InsertEnter",
         config = true,
-        disable_filetype = { "tex" },
+        opts = {
+            disable_filetype = { "TelescopePrompt", "spectre_panel", "tex", "latex" },
+        },
         -- use opts = {} for passing setup options
         -- this is equalent to setup({}) function
     },
