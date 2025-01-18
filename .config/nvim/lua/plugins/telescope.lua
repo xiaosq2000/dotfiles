683261bcbe930773ab9be3f3d472da0e4afdd1fe
Shuqi XIAO <xiaosq2000@gmail.com>
2025-01-18 10:57:30 +0000

    Update

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -16,5 +16,48 @@ return {
         vim.keymap.set('n', '<space>fhp', builtin.help_tags, {})
         --
         require("telescope").load_extension("ui-select")
+        -- Insert file path
+        local telescope = require('telescope.builtin')
+        local actions = require('telescope.actions')
+        local action_state = require('telescope.actions.state')
+
+        -- Custom function to insert filepath at cursor
+        local function insert_filepath(filepath)
+            -- Get current cursor position
+            local pos = vim.api.nvim_win_get_cursor(0)
+            local row = pos[1] - 1 -- Convert to 0-based index
+            local col = pos[2]
+
+            -- Get current line
+            local line = vim.api.nvim_buf_get_lines(0, row, row + 1, false)[1]
+
+            -- Insert filepath at cursor position
+            local new_line = string.sub(line, 1, col) .. filepath .. string.sub(line, col + 1)
+            vim.api.nvim_buf_set_lines(0, row, row + 1, false, { new_line })
+
+            -- Move cursor to end of inserted filepath
+            vim.api.nvim_win_set_cursor(0, { row + 1, col + #filepath })
+        end
+
+        -- Custom file search function
+        local function file_search_and_insert()
+            telescope.find_files({
+                attach_mappings = function(prompt_bufnr, map)
+                    -- Override default select action
+                    actions.select_default:replace(function()
+                        actions.close(prompt_bufnr)
+                        local selection = action_state.get_selected_entry()
+                        if selection then
+                            insert_filepath(selection.value)
+                        end
+                    end)
+                    return true
+                end,
+            })
+        end
+
+        -- Set up the keybinding
+        vim.keymap.set('n', '<leader>fi', file_search_and_insert,
+            { noremap = true, silent = true, desc = 'Search file and insert path' })
     end
 }
