28870e4a152a3b50ddffb1476143fcf6fb757143
Shuqi XIAO <xiaosq2000@gmail.com>
2025-03-06 17:22:00 +0800

    Disable neovim plugin 'peek' inside Docker

--- .config/nvim/lua/plugins/peek.lua
+++ .config/nvim/lua/plugins/peek.lua
@@ -1,11 +1,24 @@
-return {
-    "fmorroni/peek.nvim",
-    branch = 'callouts',
-    event = { "VeryLazy" },
-    build = "deno task --quiet build:fast",
-    config = function()
-        require("peek").setup({ app = 'browser' })
-        vim.api.nvim_create_user_command("PeekOpen", require("peek").open, {})
-        vim.api.nvim_create_user_command("PeekClose", require("peek").close, {})
-    end,
-}
+local function is_docker()
+    local f = io.open("/.dockerenv", "r")
+    if f ~= nil then
+        io.close(f)
+        return true
+    end
+    return os.getenv("container") == "docker"
+end
+
+if not is_docker() then
+    return {
+        "fmorroni/peek.nvim",
+        branch = 'callouts',
+        event = { "VeryLazy" },
+        build = "deno task --quiet build:fast",
+        config = function()
+            require("peek").setup({ app = 'browser' })
+            vim.api.nvim_create_user_command("PeekOpen", require("peek").open, {})
+            vim.api.nvim_create_user_command("PeekClose", require("peek").close, {})
+        end,
+    }
+end
+
+return {}
