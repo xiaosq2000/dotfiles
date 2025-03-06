4ae870811ca5c2a5ee9ef71d4cffdcca5c5b82c6
Shuqi XIAO <xiaosq2000@gmail.com>
2025-03-06 16:28:31 +0800

    Update telescope shortcut keymaps
    Use <leader> instead of <space> to avoid conflicts with more frequent
    '<space>f' for LSP formatting.

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -6,14 +6,14 @@ return {
     config = function()
         local builtin = require('telescope.builtin')
         --
-        vim.keymap.set('n', '<space>fs', function()
+        vim.keymap.set('n', '<leader>fs', function()
             builtin.grep_string({ search = vim.fn.input("Grep > ") });
         end)
         --
-        vim.keymap.set('n', '<space>ff', builtin.find_files, {})
-        vim.keymap.set('n', '<space>fg', builtin.live_grep, {})
-        vim.keymap.set('n', '<space>fb', builtin.buffers, {})
-        vim.keymap.set('n', '<space>fhp', builtin.help_tags, {})
+        vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+        vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+        vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+        vim.keymap.set('n', '<leader>fhp', builtin.help_tags, {})
         --
         require("telescope").load_extension("ui-select")
         -- Insert file path
