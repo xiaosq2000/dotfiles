67a92fb89cc1d736e898188ba318c2fc5ab4d208
Shuqi XIAO <xiaosq2000@gmail.com>
2025-03-09 17:41:06 +0800

    Add yazi plugin 'bookmarks'

---
+++ .config/yazi/init.lua
@@ -0,0 +1,18 @@
+-- ~/.config/yazi/init.lua
+require("bookmarks"):setup({
+    -- https://github.com/dedukun/bookmarks.yazi
+    last_directory = { enable = false, persist = false, mode = "dir" },
+    persist = "vim",
+    desc_format = "parent",
+    file_pick_mode = "hover",
+    custom_desc_input = false,
+    notify = {
+        enable = true,
+        timeout = 1,
+        message = {
+            new = "New bookmark '<key>' -> '<folder>'",
+            delete = "Deleted bookmark in '<key>'",
+            delete_all = "Deleted all bookmarks",
+        },
+    },
+})
