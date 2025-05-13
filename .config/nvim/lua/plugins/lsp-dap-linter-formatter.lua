7628b1c7de38bec308cc90171fd852da82d64943
Shuqi XIAO <xiaosq2000@gmail.com>
2025-05-13 16:26:45 +0800

    feat(neovim): bump to 0.11 and change keymappings

--- .config/nvim/lua/plugins/lsp-dap-linter-formatter.lua
+++ .config/nvim/lua/plugins/lsp-dap-linter-formatter.lua
@@ -25,9 +25,14 @@ return {
 					"docker_compose_language_service",
 					"jsonls",
 					"taplo",
-					-- "texlab", # BUG:
+					-- "texlab", # BUG: w/ vimtex
 				},
 			})
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
 			vim.api.nvim_create_autocmd("LspAttach", {
 				group = vim.api.nvim_create_augroup("lsp_attach_disable_ruff_hover", { clear = true }),
 				callback = function(args)
@@ -42,7 +47,7 @@ return {
 				end,
 				desc = "LSP: Disable hover capability from Ruff",
 			})
-			require("lspconfig").pyright.setup({
+			vim.lsp.config("pyright", {
 				settings = {
 					pyright = {
 						-- Using Ruff's import organizer
@@ -56,53 +61,75 @@ return {
 					},
 				},
 			})
-			-- local on_attach = function(client)
-			-- 	if client.name == "ruff" then
-			-- 		-- Disable hover in favor of Pyright
-			-- 		client.server_capabilities.hoverProvider = false
-			-- 	end
-			-- end
-			-- require("mason-lspconfig").setup_handlers({
-			-- 	-- The first entry (without a key) will be the default handler
-			-- 	-- and will be called for each installed server that doesn't have
-			-- 	-- a dedicated handler.
-			-- 	function(server_name) -- default handler (optional)
-			-- 		require("lspconfig")[server_name].setup({})
-			-- 	end,
-			-- 	["lua_ls"] = function()
-			-- 		require("lspconfig").lua_ls.setup({
-			-- 			settings = {
-			-- 				Lua = {
-			-- 					diagnostics = {
-			-- 						globals = { "vim" },
-			-- 					},
-			-- 				},
-			-- 			},
-			-- 		})
-			-- 	end,
-			-- 	["ruff"] = function()
-			-- 		require("lspconfig").ruff.setup({
-			-- 			on_attach = on_attach,
-			-- 		})
-			-- 	end,
-			-- 	["pyright"] = function()
-			-- 		require("lspconfig").pyright.setup({
-			-- 			on_attach = on_attach,
-			-- 			settings = {
-			-- 				pyright = {
-			-- 					-- Using Ruff's import organizer
-			-- 					disableOrganizeImports = true,
-			-- 				},
-			-- 				python = {
-			-- 					analysis = {
-			-- 						-- Ignore all files for analysis to exclusively use Ruff for linting
-			-- 						ignore = { "*" },
-			-- 					},
-			-- 				},
-			-- 			},
-			-- 		})
-			-- 	end,
-			-- })
+			-- Use LspAttach autocommand to only map the following keys
+			-- after the language server attaches to the current buffer
+			vim.api.nvim_create_autocmd("LspAttach", {
+				group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+				callback = function()
+					require("which-key").add({
+						{ "K", vim.lsp.buf.hover },
+						{ "grk", vim.lsp.buf.signature_help, desc = "vim.lsp.buf.signature_help" },
+						{ "grD", vim.lsp.buf.declaration, desc = "vim.lsp.buf.declaration" },
+						{
+							"<leader>wa",
+							vim.lsp.buf.add_workspace_folder,
+							desc = "vim.lsp.buf.add_workspace_folder",
+						},
+						{
+							"<leader>wr",
+							vim.lsp.buf.remove_workspace_folder,
+							desc = "vim.lsp.buf.remove_workspace_folder",
+						},
+						{
+							"<leader>wl",
+							function()
+								print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+							end,
+						},
+					})
+				end,
+			})
+		end,
+		keys = {
+			{
+				"<space>q",
+				vim.diagnostic.setloclist,
+				desc = "Set diagnostics in location list",
+			},
+			{
+				"<space>e",
+				vim.diagnostic.open_float,
+				desc = "Open floating diagnostic window",
+			},
+			{
+				"<space>co",
+				":copen<CR>",
+				desc = "Open quickfix window",
+			},
+			{
+				"<space>cx",
+				":cclose<CR>",
+				desc = "Close quickfix window",
+			},
+		},
+	},
+	{
+		-- A pretty preview window for Neovim that provides VSCode-like peek preview functionality for LSP locations. Glance enables you to preview, navigate, and edit LSP-provided code locations without leaving your current context.
+		"dnlhc/glance.nvim",
+		dependencies = { "neovim/nvim-lspconfig" },
+		cmd = "Glance",
+		config = function()
+			vim.api.nvim_create_autocmd("LspAttach", {
+				group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+				callback = function()
+					require("which-key").add(
+						{ "grd", "<CMD>Glance definitions<CR>", desc = "vim.lsp.buf.definitions()" },
+						{ "grr", "<CMD>Glance references<CR>", desc = "vim.lsp.buf.references()" },
+						{ "gry", "<CMD>Glance type_definitions<CR>", desc = "vim.lsp.buf.type_definitions()" },
+						{ "gri", "<CMD>Glance implementations<CR>", desc = "vim.lsp.buf.implementations()" }
+					)
+				end,
+			})
 		end,
 	},
 	{
@@ -144,127 +171,6 @@ return {
 	{
 		"zapling/mason-conform.nvim",
 		dependencies = { "stevearc/conform.nvim", "williamboman/mason.nvim" },
-	},
-	{
-		"neovim/nvim-lspconfig",
-		dependencies = { "saghen/blink.cmp", "kevinhwang91/nvim-ufo" },
-		-- example using `opts` for defining servers
-		opts = {
-			servers = {
-				lua_ls = {
-					settings = {
-						Lua = {
-							diagnostics = {
-								globals = { "vim" }, -- Recognize vim global
-							},
-						},
-					},
-				},
-			},
-		},
-		config = function(_, opts)
-			local lspconfig = require("lspconfig")
-			local capabilities = vim.lsp.protocol.make_client_capabilities()
-			capabilities.textDocument.foldingRange = {
-				dynamicRegistration = false,
-				lineFoldingOnly = true,
-			}
-			for server, config in pairs(opts.servers) do
-				-- passing config.capabilities to blink.cmp merges with the capabilities in your
-				-- `opts[server].capabilities, if you've defined it
-				config.capabilities = capabilities
-				config.capabilities = require("blink.cmp").get_lsp_capabilities(config.capabilities)
-				lspconfig[server].setup(config)
-			end
-
-			-- Use LspAttach autocommand to only map the following keys
-			-- after the language server attaches to the current buffer
-			vim.api.nvim_create_autocmd("LspAttach", {
-				group = vim.api.nvim_create_augroup("UserLspConfig", {}),
-				callback = function()
-					-- Changed variable name from opts to buffer_opts
-					local wk = require("which-key")
-					wk.add({
-						{ "K", vim.lsp.buf.hover },
-						{ "gK", vim.lsp.buf.signature_help },
-						{ "gD", vim.lsp.buf.declaration },
-						{ "gd", vim.lsp.buf.definition },
-						{ "gY", vim.lsp.buf.type_definition },
-						{ "gR", vim.lsp.buf.references },
-						{ "gM", vim.lsp.buf.implementation },
-						{ "<space>rn", vim.lsp.buf.rename },
-						{
-							"<space>f",
-							function()
-								vim.lsp.buf.format({ async = true })
-							end,
-						},
-						{ "<space>wa", vim.lsp.buf.add_workspace_folder },
-						{ "<space>wr", vim.lsp.buf.remove_workspace_folder },
-						{
-							"<space>wl",
-							function()
-								print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-							end,
-						},
-						{ "<space>ca", vim.lsp.buf.code_action, mode = { "n", "v" } },
-					})
-				end,
-			})
-		end,
-		keys = {
-			{
-				"<space>q",
-				vim.diagnostic.setloclist,
-				desc = "Set diagnostics in location list",
-			},
-			{
-				"<space>e",
-				vim.diagnostic.open_float,
-				desc = "Open floating diagnostic window",
-			},
-			{
-				"[d",
-				vim.diagnostic.goto_prev,
-				desc = "Go to previous diagnostic",
-			},
-			{
-				"]d",
-				vim.diagnostic.goto_next,
-				desc = "Go to next diagnostic",
-			},
-			{
-				"<space>co",
-				":copen<CR>",
-				desc = "Open quickfix window",
-			},
-			{
-				"<space>cx",
-				":cclose<CR>",
-				desc = "Close quickfix window",
-			},
-			{
-				"[q",
-				":cprevious<CR>",
-				desc = "Previous quickfix entry",
-			},
-			{
-				"]q",
-				":cnext<CR>",
-				desc = "Next quickfix entry",
-			},
-		},
-	},
-	{
-		-- A pretty preview window for Neovim that provides VSCode-like peek preview functionality for LSP locations. Glance enables you to preview, navigate, and edit LSP-provided code locations without leaving your current context.
-		"dnlhc/glance.nvim",
-		cmd = "Glance",
-		keys = {
-			{ "gd", "<CMD>Glance definitions<CR>" },
-			{ "gr", "<CMD>Glance references<CR>" },
-			{ "gy", "<CMD>Glance type_definitions<CR>" },
-			{ "gm", "<CMD>Glance implementations<CR>" },
-		},
 	},
 	{
 		"mfussenegger/nvim-lint",
--- .config/nvim/lua/plugins/nvim-ufo.lua
+++ .config/nvim/lua/plugins/nvim-ufo.lua
@@ -1,45 +1,60 @@
 return {
-	"kevinhwang91/nvim-ufo",
-	dependencies = { "kevinhwang91/promise-async" },
-	config = function()
-		vim.o.foldcolumn = "1" -- '0' is not bad
-		vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
-		vim.o.foldlevelstart = 99
-		vim.o.foldenable = true
-		-- vim.o.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
-		local handler = function(virtText, lnum, endLnum, width, truncate)
-			local newVirtText = {}
-			local suffix = (" 󰁂 %d "):format(endLnum - lnum)
-			local sufWidth = vim.fn.strdisplaywidth(suffix)
-			local targetWidth = width - sufWidth
-			local curWidth = 0
-			for _, chunk in ipairs(virtText) do
-				local chunkText = chunk[1]
-				local chunkWidth = vim.fn.strdisplaywidth(chunkText)
-				if targetWidth > curWidth + chunkWidth then
-					table.insert(newVirtText, chunk)
-				else
-					chunkText = truncate(chunkText, targetWidth - curWidth)
-					local hlGroup = chunk[2]
-					table.insert(newVirtText, { chunkText, hlGroup })
-					chunkWidth = vim.fn.strdisplaywidth(chunkText)
-					-- str width returned from truncate() may less than 2nd argument, need padding
-					if curWidth + chunkWidth < targetWidth then
-						suffix = suffix .. (" "):rep(targetWidth - curWidth - chunkWidth)
+	{
+		"kevinhwang91/nvim-ufo",
+		enabled = vim.env.KITTY_SCROLLBACK_NVIM ~= "true",
+		dependencies = { "kevinhwang91/promise-async", "neovim/nvim-lspconfig", "rmagatti/auto-session" },
+		config = function()
+			vim.o.foldcolumn = "1" -- '0' is not bad
+			vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
+			vim.o.foldlevelstart = 99
+			vim.o.foldenable = true
+			-- vim.o.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
+			local handler = function(virtText, lnum, endLnum, width, truncate)
+				local newVirtText = {}
+				local suffix = (" 󰁂 %d "):format(endLnum - lnum)
+				local sufWidth = vim.fn.strdisplaywidth(suffix)
+				local targetWidth = width - sufWidth
+				local curWidth = 0
+				for _, chunk in ipairs(virtText) do
+					local chunkText = chunk[1]
+					local chunkWidth = vim.fn.strdisplaywidth(chunkText)
+					if targetWidth > curWidth + chunkWidth then
+						table.insert(newVirtText, chunk)
+					else
+						chunkText = truncate(chunkText, targetWidth - curWidth)
+						local hlGroup = chunk[2]
+						table.insert(newVirtText, { chunkText, hlGroup })
+						chunkWidth = vim.fn.strdisplaywidth(chunkText)
+						-- str width returned from truncate() may less than 2nd argument, need padding
+						if curWidth + chunkWidth < targetWidth then
+							suffix = suffix .. (" "):rep(targetWidth - curWidth - chunkWidth)
+						end
+						break
 					end
-					break
+					curWidth = curWidth + chunkWidth
 				end
-				curWidth = curWidth + chunkWidth
+				table.insert(newVirtText, { suffix, "MoreMsg" })
+				return newVirtText
 			end
-			table.insert(newVirtText, { suffix, "MoreMsg" })
-			return newVirtText
-		end
-		require("ufo").setup({
-			fold_virt_text_handler = handler,
-		})
-	end,
-	keys = {
-		{ "zR", require("ufo").openAllFolds },
-		{ "zM", require("ufo").closeAllFolds },
+			local capabilities = vim.lsp.protocol.make_client_capabilities()
+			capabilities.textDocument.foldingRange = {
+				dynamicRegistration = false,
+				lineFoldingOnly = true,
+			}
+			local language_servers = vim.lsp.get_clients() -- or list servers manually like {'gopls', 'clangd'}
+			for _, ls in ipairs(language_servers) do
+				require("lspconfig")[ls].setup({
+					capabilities = capabilities,
+					-- you can add other fields for setting up lsp server in this table
+				})
+			end
+			require("ufo").setup({
+				fold_virt_text_handler = handler,
+			})
+			require("which-key").add(
+				{ "zR", require("ufo").openAllFolds, desc = "Open all folds" },
+				{ "zM", require("ufo").closeAllFolds, desc = "Close all folds" }
+			)
+		end,
 	},
 }
