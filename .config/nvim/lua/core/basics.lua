519caa035cc4442f708ff69a7f5c9919ee303271
Shuqi XIAO <xiaosq2000@gmail.com>
2025-07-24 14:03:19 +0800

    feat(neovim): Update foldtext

--- .config/nvim/lua/core/basics.lua
+++ .config/nvim/lua/core/basics.lua
@@ -24,7 +24,7 @@ vim.keymap.set("n", "<leader>yp", ":let @+ = expand('%:p')<cr>", opts)
 vim.keymap.set("n", "<leader>yr", ":let @+ = expand('%')<cr>", opts)
 
 -- comment block, ref: https://vi.stackexchange.com/a/421
--- TODO: adapt to indent elegantly
+-- TODO: use luasnip; adapt to indent elegantly
 vim.keymap.set({ "n", "i" }, "<leader>c/", "<esc><cmd>center 80<cr>hhv0r/A<space><esc>40A/<esc>d80<bar>YppVr/kk.", opts)
 vim.keymap.set({ "n", "i" }, "<leader>c%", "<esc><cmd>center 80<cr>hhv0r%A<space><esc>40A%<esc>d80<bar>YppVr%kk.", opts)
 vim.keymap.set({ "n", "i" }, "<leader>c-", "<esc><cmd>center 80<cr>hhv0r-A<space><esc>40A-<esc>d80<bar>YppVr-kk.", opts)
@@ -83,8 +83,8 @@ vim.opt.laststatus = 3
 --------------------------------- diagnostics ----------------------------------
 --------------------------------------------------------------------------------
 -- vim.diagnostic.config({
---     virtual_text = true,
---     virtual_lines = true
+-- 	virtual_text = true,
+-- 	virtual_lines = true,
 -- })
 
 --------------------------------------------------------------------------------
@@ -93,18 +93,57 @@ vim.opt.laststatus = 3
 vim.o.foldenable = true
 vim.o.foldlevel = 99
 vim.o.foldmethod = "expr"
-vim.o.foldtext = ""
-vim.opt.foldcolumn = "0"
-vim.opt.fillchars:append({ fold = " " })
+vim.o.foldcolumn = "0"
+
 -- Default to treesitter folding
-vim.o.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+vim.o.foldexpr = "v:lua.vim.treesitter.foldexpr()"
 -- Prefer LSP folding if client supports it
-vim.api.nvim_create_autocmd('LspAttach', {
-    callback = function(args)
-        local client = vim.lsp.get_client_by_id(args.data.client_id)
-        if client and client:supports_method('textDocument/foldingRange') then
-            vim.wo.foldexpr = 'v:lua.vim.lsp.foldexpr()'
-            vim.wo.foldmethod = 'expr'
-        end
-    end,
+vim.api.nvim_create_autocmd("LspAttach", {
+	callback = function(args)
+		local client = vim.lsp.get_client_by_id(args.data.client_id)
+		if client and client:supports_method("textDocument/foldingRange") then
+			vim.wo.foldexpr = "v:lua.vim.lsp.foldexpr()"
+			vim.wo.foldmethod = "expr"
+		end
+	end,
 })
+
+--- Reference: https://www.reddit.com/r/neovim/comments/1fzn1zt/custom_fold_text_function_with_treesitter_syntax
+local function fold_virt_text(result, s, lnum, coloff)
+	if not coloff then
+		coloff = 0
+	end
+	local text = ""
+	local hl
+	for i = 1, #s do
+		local char = s:sub(i, i)
+		local hls = vim.treesitter.get_captures_at_pos(0, lnum, coloff + i - 1)
+		local _hl = hls[#hls]
+		if _hl then
+			local new_hl = "@" .. _hl.capture
+			if new_hl ~= hl then
+				table.insert(result, { text, hl })
+				text = ""
+				hl = nil
+			end
+			text = text .. char
+			hl = new_hl
+		else
+			text = text .. char
+		end
+	end
+	table.insert(result, { text, hl })
+end
+
+function _G.custom_foldtext()
+	local start = vim.fn.getline(vim.v.foldstart):gsub("\t", string.rep(" ", vim.o.tabstop))
+	local end_str = vim.fn.getline(vim.v.foldend)
+	local end_ = vim.trim(end_str)
+	local result = {}
+	fold_virt_text(result, start, vim.v.foldstart - 1)
+	table.insert(result, { " ... ", "Delimiter" })
+	fold_virt_text(result, end_, vim.v.foldend - 1, #(end_str:match("^(%s+)") or ""))
+	return result
+end
+
+vim.opt.foldtext = "v:lua.custom_foldtext()"
