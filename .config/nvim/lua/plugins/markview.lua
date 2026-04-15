0a45d2e4d91749d7af4757c3dda79d62aea91160
Shuqi XIAO <xiaosq2000@gmail.com>
2026-04-15 17:00:05 +0800

    feat(nvim): tighten markview markdown list indentation

--- .config/nvim/lua/plugins/markview.lua
+++ .config/nvim/lua/plugins/markview.lua
@@ -2,6 +2,13 @@ return {
     "OXY2DEV/markview.nvim",
     lazy = false,
     dependencies = { "saghen/blink.cmp" },
+    opts = {
+        markdown = {
+            list_items = {
+                shift_width = 2,
+            },
+        },
+    },
     init = function()
         vim.api.nvim_create_autocmd("FileType", {
             pattern = "markdown",
