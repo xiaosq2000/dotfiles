a883373975523794da72444c43069923223a3252
Shuqi XIAO <xiaosq2000@gmail.com>
2026-04-15 17:02:27 +0800

    feat(nvim): add keymap to toggle markview mode

--- .config/nvim/lua/plugins/markview.lua
+++ .config/nvim/lua/plugins/markview.lua
@@ -1,20 +1,26 @@
 return {
-    "OXY2DEV/markview.nvim",
-    lazy = false,
-    dependencies = { "saghen/blink.cmp" },
-    opts = {
-        markdown = {
-            list_items = {
-                shift_width = 2,
-            },
-        },
-    },
-    init = function()
-        vim.api.nvim_create_autocmd("FileType", {
-            pattern = "markdown",
-            callback = function()
-                vim.opt_local.wrap = false
-            end,
-        })
-    end,
-};
+	"OXY2DEV/markview.nvim",
+	lazy = false,
+	dependencies = { "saghen/blink.cmp" },
+	opts = {
+		markdown = {
+			list_items = {
+				shift_width = 2,
+			},
+		},
+	},
+	init = function()
+		vim.api.nvim_create_autocmd("FileType", {
+			pattern = "markdown",
+			callback = function()
+				vim.opt_local.wrap = false
+			end,
+		})
+		vim.api.nvim_set_keymap(
+			"n",
+			"<leader>m",
+			"<CMD>Markview<CR>",
+			{ desc = "Toggles `markview` previews globally." }
+		)
+	end,
+}
