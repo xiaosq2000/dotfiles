721433daab8cff4e1192df2a1bf5c9d87eca0b66
Shuqi XIAO <xiaosq2000@gmail.com>
2026-04-20 19:08:51 +0800

    feat(nvim): add texlab LaTeX LSP
    Enable native LaTeX language features while keeping vimtex in charge of compilation and viewing to avoid overlapping workflows.

---
+++ .config/nvim/after/lsp/texlab.lua
@@ -0,0 +1,14 @@
+return {
+	settings = {
+		texlab = {
+			build = {
+				onSave = false,
+				forwardSearchAfter = false,
+			},
+			chktex = {
+				onEdit = false,
+				onOpenAndSave = false,
+			},
+		},
+	},
+}
