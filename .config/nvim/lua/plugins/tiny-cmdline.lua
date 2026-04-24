e31182b2812b4c209b5cb2a8e94b1e034df318d1
Shuqi XIAO <xiaosq2000@gmail.com>
2026-04-24 18:27:03 +0800

    feat(nvim): configure ui2 message routing

--- .config/nvim/lua/plugins/tiny-cmdline.lua
+++ .config/nvim/lua/plugins/tiny-cmdline.lua
@@ -2,7 +2,6 @@ return {
 	"rachartier/tiny-cmdline.nvim",
 	event = "VeryLazy",
 	config = function()
-		vim.o.cmdheight = 0
 		require("tiny-cmdline").setup({
 			on_reposition = require("tiny-cmdline").adapters.blink,
 		})
